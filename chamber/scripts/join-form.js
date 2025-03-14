5b4b8e5d3de0eb76911136d9af8a2cb6ba1fd4f8
Brigham Dean <Briggs-dean@outlook.com>
2025-03-14 13:29:26 -0800

    moar pagees

---
+++ chamber/scripts/join-form.js
@@ -0,0 +1,75 @@
+// Grab the current URL for this page
+// including the attached GET values
+const currentURL = window.location.href;
+console.log(currentURL);
+
+// decide what split character you will use for the CurrentURL variable
+const everything = currentURL.split('?');
+console.log(everything);
+
+// grab just the second half
+let formData = everything[1];
+console.log(formData);
+
+// break the form name value pairs into an array
+formData = formData.split('&');
+console.log(formData);
+
+function show(cup) {
+    console.log(cup);
+
+    formData.forEach((element) => {
+        // ask if each element starts with the parameter we sent through the function (in this case, cup)
+        if (element.startsWith(cup)) {
+            // cut the, for eg: 'phone=' off
+            // and now only show what is after, for eg: 'last.' (do this using [1])
+            // .replace("%40", "@") fixes the coding for "@" that takes place when data is stored in a URL
+            result = element.split("=")[1].replace("%40", "@");
+            console.log(result);
+
+        } // end if
+    })
+    return (result);
+} // end show
+
+
+// function to replace multiple substrings in the timestamp url value
+function timestampReplace(timestamp) {
+    timestamp = timestamp.replace(/%2F/g, "/");
+    timestamp = timestamp.replace(/%2C/g, " ");
+    timestamp = timestamp.replace(/%3A/g, ":")
+    timestamp = timestamp.replace(/\+/g, " ");
+    return timestamp;
+}
+
+
+
+
+
+
+
+const showInfo = document.querySelector('#results');
+console.log(showInfo.innerHTML);
+showInfo.innerHTML = `
+<p>Form submitted by: ${show('first')} ${show('last')}</p>
+<p>Email: <a href="mailto:${show("email")}">${show("email")}</a></p>
+<p>Phone number: ${show("telephone")}</p>
+<p>Organization: ${show("organization").replace("+", " ")}</p>
+<p>Membership level selected: ${show("membershipLevel")}</p>
+<p>Application submitted: ${timestampReplace(show("timestamp"))}</p>
+`;
+
+// timestamp will show up something like this:
+// 12%2F10%2F2024%2C+1%3A13%3A33+AM
+
+
+
+
+
+
+
+
+
+//const str = 'Enter to Learn; Go Forth to Serve';
+//console.log(str.replace('Go forth', 'Graduate'));
+// Enter to Learn; Graduate to Serve
---
+++ chamber/scripts/spotlight.js
@@ -0,0 +1,86 @@
+const source = 'members.json'
+const cards = document.querySelector('#spotlight');
+
+// Create an async defined function named "getMemberData" 
+// to fetch data from the JSON source url using the await 
+// fetch() method.
+
+async function getMemberData() {
+    const response = await fetch(source);
+    const data = await response.json();
+    //console.table(data.members);
+    displayMembers(data.members);
+}
+
+getMemberData();
+function getRandomMembers(array, numMembers) {
+    const result = []; // This is the array where the function will store the members
+    const selectedIndices = []; // This array will track the indices (indexes) we have already used so we don't have duplicates
+
+    //while we still don't have the number of Members we need:
+    while (result.length < numMembers) {
+        // generate a random index using Math
+        const randomIndex = Math.floor(Math.random() * array.length); // Math.floor makes sure that the random number rounds down so that it always is within the array's indices
+
+        // if this index hasn't already been selected, then 1.) add it to the randomly chosen members array, 2.) add it now to the selectedIndices array so it won't be chosen next time
+        if (!selectedIndices.includes(randomIndex)) {
+            result.push(array[randomIndex]);
+            selectedIndices.push(randomIndex);
+        }
+    }
+
+    return result; // This will return the array of randomly chosen members
+}
+
+const displayMembers = (members) => {
+    // card build goes here
+    cards.innerHTML = " ";
+    // get random members
+    const randomMembers = getRandomMembers(members, 3);
+    randomMembers.forEach((randomMember) => {
+        let card = document.createElement("section");
+        let companyName = document.createElement('h3');
+        let address = document.createElement('p');
+        //address.classList.add('address'); // add class for CSS
+        let phoneNumber = document.createElement('p');
+        //phoneNumber.classList.add('phone-number'); // add class for CSS
+        let url = document.createElement('p');
+        //url.classList.add('url'); // add class for CSS
+        let logo = document.createElement('img');
+        let membershipLevel = document.createElement('p');
+
+        // <h2> for companyName
+        companyName.textContent = `${randomMember.name}`;
+
+        // <p> for phoneNumber
+        phoneNumber.textContent = `PHONE: ${randomMember.phoneNumber}`;
+
+        // <p> for address
+        address.textContent = `ADDRESS: ${randomMember.address}`;
+
+        // <p> for URL
+        url.innerHTML = `
+            <a href="${randomMember.url}">Website</a>
+        `;
+
+        // <img> attributes for logo
+        logo.setAttribute('src', randomMember.imgFile);
+        logo.setAttribute('alt', `Business logo for ${randomMember.name}.`);
+        logo.setAttribute('loading', 'lazy');
+        //logo.setAttribute('width', '150');
+        //logo.setAttribute('height', '75');
+
+        // <p> for membershipLevel
+        membershipLevel.textContent = `${randomMember.membershipLevel} Member`;
+
+        // append so they show up on card
+        card.appendChild(logo);
+        card.appendChild(companyName);
+        card.appendChild(phoneNumber);
+        card.appendChild(address);
+        card.appendChild(url);
+        card.appendChild(membershipLevel);
+
+        cards.appendChild(card);
+    })
+}
