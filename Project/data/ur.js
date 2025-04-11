809541bad5b86089dfffd70bd1b8ebb05f53ae92
Brigham Dean <Briggs-dean@outlook.com>
2025-04-11 11:07:56 -0800

    tst7

--- Project/data/ur.js
+++ Project/data/ur.js
@@ -1,11 +1,25 @@
-const openBtn = document.getElementById("openModal");
-const closeBtn = document.getElementById("closeModal");
-const modal = document.getElementById("modal");
+// Get the modal
+var modal = document.getElementById("myModal");
 
-openBtn.addEventListener("click", () => {
-    modal.classList.add("open");
-});
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-CloseBtn.addEventListener("click", () => {
-    modal.classList.remove("open");
-});
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
