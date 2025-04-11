99e4f2a01e6d7e6f350bac53227578f0b7a41de0
Brigham Dean <Briggs-dean@outlook.com>
2025-04-11 11:52:15 -0800

    js3

--- Project/data/Final_Project.js
+++ Project/data/Final_Project.js
@@ -456,15 +456,3 @@ document.addEventListener("DOMContentLoaded", () => {
         UrdragonCard.addEventListener("click", () => changeColor('The Ur-dragon'));
     }
 });
-
-let currentYear = document.querySelector("#currentyear");
-
-const todaysDate = new Date();
-
-currentYear.innerHTML = `<span class="currentYear">${todaysDate.getFullYear()}</span>`
-
-let lastMod = document.querySelector("#lastModified");
-
-const dateLastMod = document.lastModified;
-
-document.getElementById("lastModified").innerHTML = `Last Modified: <span class="lastMod">${dateLastMod}</span>`
